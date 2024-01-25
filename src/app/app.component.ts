ad92257bf250771e18e862f3804795b02a2b3357
pawansha23 <pawanakg@gmail.com>
2024-01-25 17:22:16 +0530

    added api calls
    added api calls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from "@angular/router";
+import { RESTAPIService } from './service/restapiservice.service';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test-ng-app';
+
+  persons : any[]=[];
+  pname: any = '';
+
+  constructor(private service: RESTAPIService, private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  getPersons() {
+    this.service.getPersons().subscribe( data => {
+      console.log("data is fetched from DB");
+      this.persons = data;
+      data.forEach(element => {
+        console.log('id = '+element.id+" , name = "+element.name);
+      });
+    });
+  }
+
+  addPerson(){
+      let person = { name:this.pname };
+  
+      this.service.addPerson(person).subscribe( data=>{
+        console.log('record saved in DB',data);
+      });
+  }
+
+  clear(){
+    this.persons = [];
+  }
+
 }
+
+
---
+++ src/app/service/restapiservice.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RESTAPIService {
+
+  constructor(private http: HttpClient) { }
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  };
+  
+  // render BE url
+  url = "https://sb-postgres.onrender.com/persons";
+
+   // local BE url
+   //url = "http://localhost:8080/persons";
+
+  getPersons() {
+    return this.http.get<any[]>(this.url);
+  }
+
+  addPerson(blog: any) {
+    return this.http.post(this.url, blog, this.httpOptions);
+  }
+
+}
